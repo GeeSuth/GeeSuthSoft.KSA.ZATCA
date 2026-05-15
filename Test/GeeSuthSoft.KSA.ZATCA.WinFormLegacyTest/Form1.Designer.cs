bb9f31216e5007525e6cc0082c2bbb2e6ddadf55
GeeSuth <servecemodea@gmail.com>
2026-05-15 22:00:39 +0300

    Add form test

--- Test/GeeSuthSoft.KSA.ZATCA.WinFormLegacyTest/Form1.Designer.cs
+++ Test/GeeSuthSoft.KSA.ZATCA.WinFormLegacyTest/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace GeeSuthSoft.KSA.ZATCA.WinFormLegacyTest
+namespace GeeSuthSoft.KSA.ZATCA.WinFormLegacyTest
 {
     partial class Form1
     {
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGenerateCsr = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerateCsr
+            // 
+            this.btnGenerateCsr.Location = new System.Drawing.Point(12, 12);
+            this.btnGenerateCsr.Name = "btnGenerateCsr";
+            this.btnGenerateCsr.Size = new System.Drawing.Size(150, 30);
+            this.btnGenerateCsr.TabIndex = 0;
+            this.btnGenerateCsr.Text = "Generate CSR";
+            this.btnGenerateCsr.UseVisualStyleBackColor = true;
+            this.btnGenerateCsr.Click += new System.EventHandler(this.btnGenerateCsr_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 48);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(776, 390);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnGenerateCsr);
+            this.Name = "Form1";
+            this.Text = "ZATCA Legacy Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerateCsr;
+        private System.Windows.Forms.TextBox txtOutput;
     }
 }
-
--- Test/GeeSuthSoft.KSA.ZATCA.WinFormLegacyTest/Form1.cs
+++ Test/GeeSuthSoft.KSA.ZATCA.WinFormLegacyTest/Form1.cs
@@ -1,20 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
+using GeeSuthSoft.KSA.ZATCA.Dto;
+using GeeSuthSoft.KSA.ZATCA.Services;
+using GeeSuthSoft.KSA.ZATCA.Helper;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using System.Net.Http;
 
 namespace GeeSuthSoft.KSA.ZATCA.WinFormLegacyTest
 {
     public partial class Form1 : Form
     {
+        private readonly IZatcaOnboardingService _zatcaOnboardingService;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Manual instantiation for testing purposes in WinForms
+            var options = new ZatcaOptions
+            {
+                LogRequestAndResponse = true
+            };
+
+            var config = new ZatcaApiConfig(Options.Create(options));
+
+            // Using NullLogger and a simple HttpClientFactory mock for manual setup
+            _zatcaOnboardingService = new ZatcaOnboardingService(
+                config,
+                new SimpleHttpClientFactory(),
+                NullLogger<ZatcaOnboardingService>.Instance
+            );
+        }
+
+        private void btnGenerateCsr_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var csrDto = new CsrGenerationDto
+                {
+                    CommonName = "TST-886431145-399999999900003",
+                    SerialNumber = "1-TST|2-TST|3-ed22f1d8-e6a2-1118-9b58-d9a8f11e445f",
+                    OrganizationIdentifier = "399999999900003",
+                    OrganizationUnitName = "Riyadh Branch",
+                    OrganizationName = "Maximum Speed Tech Supply LTD",
+                    CountryName = "SA",
+                    InvoiceType = "1100",
+                    LocationAddress = "RRRD2929",
+                    IndustryBusinessCategory = "Supply activities"
+                };
+
+                var result = _zatcaOnboardingService.GenerateCsr(csrDto, true);
+                txtOutput.Text = $"CSR Generated Successfully:\r\n\r\n{result.Csr}\r\n\r\nPrivate Key:\r\n{result.PrivateKey}";
+            }
+            catch (Exception ex)
+            {
+                txtOutput.Text = $"Error: {ex.Message}\r\n{ex.StackTrace}";
+            }
+        }
+    }
+
+    // Simple mock for IHttpClientFactory
+    public class SimpleHttpClientFactory : IHttpClientFactory
+    {
+        public HttpClient CreateClient(string name)
+        {
+            return new HttpClient();
         }
     }
 }
